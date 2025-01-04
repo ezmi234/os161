7c50fbbaf38632e028668a7f3aa84d327de026b5
ezmi234 <ezmirondeniku@gmail.com>
2025-01-04 16:23:34 +0100

    fix(waitpid): CHECKING IF THE PROCESS IS A CHILD

--- kern/include/syscall.h
+++ kern/include/syscall.h
@@ -73,7 +73,7 @@ int sys_dup2(int oldf, int newfd);
 int sys_chdir(const char *path);
 char *sys_getcwd(char buf[], size_t size);
 pid_t sys_getpid(void);
-int sys_waitpid(pid_t pid, int *status, int options, pid_t *retval);
+int sys_waitpid(pid_t pid, int *status, int options, int *retval);
 int sys_fork(struct trapframe *tf, pid_t *retval);
 int sys_execv(const char *program, char **args);
 #endif
