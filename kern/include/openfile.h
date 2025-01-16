3880a319a8359a3adf89873e7006f95cc7afba1a
fedspi00 <s324617@studenti.polito.it>
2025-01-16 22:56:18 +0100

    docs: openfile struct comments

--- kern/include/openfile.h
+++ kern/include/openfile.h
@@ -4,12 +4,24 @@
 #include <vnode.h>
 #include <types.h>
 
+/**
+ * @struct openfile
+ * 
+ * @brief Represents an open file in the OS/161 kernel.
+ * 
+ * This structure contains information about an open file, including:
+ * - A pointer to the vnode representing the file.
+ * - A lock to ensure thread-safe access to the file.
+ * - A reference count indicating how many times the file is open.
+ * - The mode in which the file was opened.
+ * - The current offset within the file.
+ */
 struct openfile {
-    struct vnode *vn;
-    off_t offset;
-    int mode;
-    int count;
-    struct lock *lock;
+    struct vnode *vn;   
+    struct lock *lock;  
+    int count;          
+    int mode;          
+    off_t offset;      
 };
 
 #endif /* _OPENFILE_H_ */
